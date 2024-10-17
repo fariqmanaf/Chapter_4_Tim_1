b0e903931452fae7d68ac2bca85baee98d545b2a
Naila Jinan Gaisani <146344930+nellnelaa@users.noreply.github.com>
2024-10-17 22:22:17 +0700

    feature: create availability (#2)

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const carsRouter = require("./cars.route");
+const availabilityRouter = require("./availability.route");
 
 const router = express.Router();
 
 router.use("/cars", carsRouter);
+router.use("/availability", availabilityRouter);
 router.use("/models", modelsRouter);
 
 module.exports = router;
