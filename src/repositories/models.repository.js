930c57a804f294aa042abd61f2a0f3aebdcc169d
Pratama <134142629+rzkyprtmaaa@users.noreply.github.com>
2024-10-18 21:55:24 +0700

    CRUD Tugas Rizky (#11)
    Co-authored-by: “rzkyprtmaaa” <“riskibkc12@gmail.com”>

---
+++ src/repositories/models.repository.js
@@ -0,0 +1,35 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+exports.createModelsRepo = async (model, type) => {
+  const newModels = await prisma.models.create({
+    data: {
+      model: data.model,
+      type: data.type,
+    },
+  });
+
+  const serializedModels = JSONBigInt.stringify(newModels);
+  return JSONBigInt.parse(serializedModels);
+};
+
+exports.updateModels = async (id, data) => {
+  const updatedModels = await prisma.models.update({
+    where: { id },
+    data,
+  });
+
+  // Convert BigInt fields to string for safe serialization
+  const serializedModels = JSONBigInt.stringify(updatedModels);
+  return JSONBigInt.parse(serializedModels);
+};
+
+exports.deleteModelsById = async (id) => {
+  const deletedModels = await prisma.models.delete({
+    where: { id },
+  });
+
+  // Convert BigInt fields to string for safe serialization
+  const serializedModels = JSONBigInt.stringify(deletedModels);
+  return JSONBigInt.parse(serializedModels);
+};
