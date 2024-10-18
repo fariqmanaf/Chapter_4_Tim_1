1bbf8cf763826d8bf9b5b7d4f08166b8f6d4c639
Pratama <134142629+rzkyprtmaaa@users.noreply.github.com>
2024-10-18 15:10:25 +0700

    FINISHED CRUD MANUFACTURES TABLE (#4)
    * feature: create models
    
    * finishing crud models
    
    * finishing crud manufactures table
    
    ---------
    
    Co-authored-by: “rzkyprtmaaa” <“riskibkc12@gmail.com”>

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const carsRouter = require("./cars.route");
-const modelsRouter = require("./models");
+const manufacturesRouter = require("./manufacture.route");
 const availabilityRouter = require("./availability.route");
 
 const router = express.Router();
 
 router.use("/cars", carsRouter);
 router.use("/availability", availabilityRouter);
-router.use("/models", modelsRouter);
+router.use("/manufactures", manufacturesRouter);
 
 module.exports = router;
