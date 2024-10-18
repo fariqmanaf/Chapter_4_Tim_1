d6f8c83cd0479aa57e904f0493980f9ca9a9cf5d
Fariq Manaf <145954724+fariqmanaf@users.noreply.github.com>
2024-10-18 15:24:30 +0700

    feat: CRUD table option_details (#5)

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -2,11 +2,14 @@ const express = require("express");
 const carsRouter = require("./cars.route");
 const manufacturesRouter = require("./manufacture.route");
 const availabilityRouter = require("./availability.route");
+const optionsRouter = require("./options.route");
 
 const router = express.Router();
 
 router.use("/cars", carsRouter);
 router.use("/availability", availabilityRouter);
+router.use("/models", modelsRouter);
+router.use("/options", optionsRouter);
 router.use("/manufactures", manufacturesRouter);
 
 module.exports = router;
